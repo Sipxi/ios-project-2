6d9cc218d0ff1440e797c3d762c9e1796f5cb939
Sipxi <sipxi@email.cz>
2025-04-18 16:15:26 +0200

    semaphors check

--- includes/main.h
+++ includes/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <time.h>
 
 // --- Argument count ---
 #define EXPECTED_ARGS 6
@@ -19,11 +27,18 @@
 #define MIN_TRUCK_ARRIVAL_US   0
 #define MAX_TRUCK_ARRIVAL_US   1000
 
+// --- Semaphore names ---
+#define SEM_FERRY "sem_ferry"        // Semaphore for ferry synchronization
+#define SEM_CARS "sem_cars"          // Semaphore for car synchronization
+#define SEM_TRUCKS "sem_trucks"      // Semaphore for truck synchronization
+#define SEM_OUTPUT "sem_output"      // Semaphore for writing to proj2.out
+
 typedef enum {
     EX_SUCCESS,
     EX_ERROR_ARGS,
 } ReturnCode;
 
+// Configuration structure
 typedef struct {
     int num_trucks;
     int num_cars;
@@ -32,6 +47,25 @@ typedef struct {
     int max_truck_arrival_us;
 } Config;
 
+// Shared data structure
+typedef struct {
+    int action_counter;       // Global action counter (A)
+    int ferry_port;           // Current port of the ferry (0 or 1)
+    int ferry_capacity;       // Remaining capacity of the ferry
+    int waiting_cars[2];      // Number of cars waiting at each port
+    int waiting_trucks[2];    // Number of trucks waiting at each port
+} SharedData;
+
+// Function prototypes
 int parse_args(int argc, char const *argv[], Config *cfg);
+void init_shared_memory(SharedData **shared_data);
+sem_t *init_semaphore(const char *name);
+void cleanup_semaphores();
+pid_t create_ferry_process(SharedData *shared_data, Config *cfg);
+void create_vehicle_processes(SharedData *shared_data, Config *cfg);
+void ferry_process(SharedData *shared_data, Config *cfg);
+void truck_process(int id, SharedData *shared_data, Config *cfg);
+void car_process(int id, SharedData *shared_data, Config *cfg);
+void cleanup_resources(SharedData *shared_data);
 
 #endif // MAIN_H
--- src/main.c
+++ src/main.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include "tests.h" 
 #include "main.h"
 
 // Function to parse arguments
@@ -27,22 +23,19 @@ int parse_args(int argc, char const *argv[], Config *cfg) {
     }
 
     cfg->capacity = strtol(argv[3], &end, 10);
-    if (*end != '\0' || cfg->capacity < MIN_CAPACITY_TRAFFIC 
-        || cfg->capacity > MAX_CAPACITY_TRAFFIC) {
+    if (*end != '\0' || cfg->capacity < MIN_CAPACITY_TRAFFIC || cfg->capacity > MAX_CAPACITY_TRAFFIC) {
         fprintf(stderr, "[ERROR] Invalid or out-of-range value for capacity: %s\n", argv[3]);
         return EX_ERROR_ARGS;
     }
 
     cfg->max_car_arrival_us = strtol(argv[4], &end, 10);
-    if (*end != '\0' || cfg->max_car_arrival_us < MIN_CAR_ARRIVAL_US 
-        || cfg->max_car_arrival_us > MAX_CAR_ARRIVAL_US) {
+    if (*end != '\0' || cfg->max_car_arrival_us < MIN_CAR_ARRIVAL_US || cfg->max_car_arrival_us > MAX_CAR_ARRIVAL_US) {
         fprintf(stderr, "[ERROR] Invalid or out-of-range value for max_car_arrival_us: %s\n", argv[4]);
         return EX_ERROR_ARGS;
     }
 
     cfg->max_truck_arrival_us = strtol(argv[5], &end, 10);
-    if (*end != '\0' || cfg->max_truck_arrival_us < MIN_TRUCK_ARRIVAL_US
-         || cfg->max_truck_arrival_us > MAX_TRUCK_ARRIVAL_US) {
+    if (*end != '\0' || cfg->max_truck_arrival_us < MIN_TRUCK_ARRIVAL_US || cfg->max_truck_arrival_us > MAX_TRUCK_ARRIVAL_US) {
         fprintf(stderr, "[ERROR] Invalid or out-of-range value for max_truck_arrival_us: %s\n", argv[5]);
         return EX_ERROR_ARGS;
     }
@@ -50,10 +43,204 @@ int parse_args(int argc, char const *argv[], Config *cfg) {
     return EX_SUCCESS;
 }
 
+// Initialize shared memory
+void init_shared_memory(SharedData **shared_data) {
+    int shm_fd = shm_open("/ferry_shared", O_CREAT | O_RDWR, 0666);
+    assert(shm_fd != -1);
 
-int main(int argc, char const *argv[]) {
-    run_all_tests();
+    ftruncate(shm_fd, sizeof(SharedData));
+    *shared_data = mmap(NULL, sizeof(SharedData), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    assert(*shared_data != MAP_FAILED);
 
-    return EX_SUCCESS;
+    // Initialize shared data
+    (*shared_data)->action_counter = 1;
+    (*shared_data)->ferry_port = 0;
+    (*shared_data)->ferry_capacity = 0;
+    (*shared_data)->waiting_cars[0] = 0;
+    (*shared_data)->waiting_cars[1] = 0;
+    (*shared_data)->waiting_trucks[0] = 0;
+    (*shared_data)->waiting_trucks[1] = 0;
 }
 
+// Initialize semaphores
+sem_t *init_semaphore(const char *name) {
+    sem_t *sem = sem_open(name, O_CREAT, 0666, 1);
+    assert(sem != SEM_FAILED);
+    return sem;
+}
+
+// Cleanup semaphores
+void cleanup_semaphores() {
+    sem_unlink(SEM_FERRY);
+    sem_unlink(SEM_CARS);
+    sem_unlink(SEM_TRUCKS);
+    sem_unlink(SEM_OUTPUT);
+}
+
+// Create ferry process
+pid_t create_ferry_process(SharedData *shared_data, Config *cfg) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        ferry_process(shared_data, cfg);
+        exit(0);
+    }
+    return pid;
+}
+
+// Create vehicle processes
+void create_vehicle_processes(SharedData *shared_data, Config *cfg) {
+    for (int i = 0; i < cfg->num_trucks; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            truck_process(i + 1, shared_data, cfg);
+            exit(0);
+        }
+    }
+
+    for (int i = 0; i < cfg->num_cars; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            car_process(i + 1, shared_data, cfg);
+            exit(0);
+        }
+    }
+}
+
+// Ferry process logic
+void ferry_process(SharedData *shared_data, Config *cfg) {
+    FILE *output = fopen("proj2.out", "w");
+    assert(output != NULL);
+
+    while (1) {
+        sem_wait(init_semaphore(SEM_FERRY));
+
+        // Arrive at the current port
+        fprintf(output, "%d: P: arrived to %d\n", shared_data->action_counter++, shared_data->ferry_port);
+
+        // Unload all vehicles
+        // TODO: Implement unloading logic
+
+        // Load waiting vehicles
+        // TODO: Implement loading logic
+
+        // Leave the current port
+        fprintf(output, "%d: P: leaving %d\n", shared_data->action_counter++, shared_data->ferry_port);
+
+        sem_post(init_semaphore(SEM_FERRY));
+
+        // Travel to the next port
+        shared_data->ferry_port = (shared_data->ferry_port + 1) % 2;
+        usleep(rand() % (cfg->max_truck_arrival_us + 1));
+
+        // Check termination condition
+        // TODO: Implement termination logic
+    }
+
+    // Finish the ferry process
+    fprintf(output, "%d: P: finish\n", shared_data->action_counter++);
+    fclose(output);
+}
+
+// Truck process logic
+void truck_process(int id, SharedData *shared_data, Config *cfg) {
+    FILE *output = fopen("proj2.out", "a");
+    assert(output != NULL);
+
+    int start_port = rand() % 2;
+    fprintf(output, "%d: N %d: started\n", shared_data->action_counter++, id);
+
+    // Travel to the starting port
+    usleep(rand() % (cfg->max_truck_arrival_us + 1));
+    fprintf(output, "%d: N %d: arrived to %d\n", shared_data->action_counter++, id, start_port);
+
+    // Wait for the ferry
+    sem_wait(init_semaphore(SEM_TRUCKS));
+    shared_data->waiting_trucks[start_port]++;
+    sem_post(init_semaphore(SEM_TRUCKS));
+
+    // Board the ferry
+    sem_wait(init_semaphore(SEM_FERRY));
+    shared_data->waiting_trucks[start_port]--;
+    shared_data->ferry_capacity--;
+    fprintf(output, "%d: N %d: boarding\n", shared_data->action_counter++, id);
+    sem_post(init_semaphore(SEM_FERRY));
+
+    // Travel to the destination port
+    int dest_port = (start_port + 1) % 2;
+
+    // Disembark
+    sem_wait(init_semaphore(SEM_FERRY));
+    fprintf(output, "%d: N %d: leaving in %d\n", shared_data->action_counter++, id, dest_port);
+    sem_post(init_semaphore(SEM_FERRY));
+
+    fclose(output);
+}
+
+// Car process logic
+void car_process(int id, SharedData *shared_data, Config *cfg) {
+    FILE *output = fopen("proj2.out", "a");
+    assert(output != NULL);
+
+    int start_port = rand() % 2;
+    fprintf(output, "%d: O %d: started\n", shared_data->action_counter++, id);
+
+    // Travel to the starting port
+    usleep(rand() % (cfg->max_car_arrival_us + 1));
+    fprintf(output, "%d: O %d: arrived to %d\n", shared_data->action_counter++, id, start_port);
+
+    // Wait for the ferry
+    sem_wait(init_semaphore(SEM_CARS));
+    shared_data->waiting_cars[start_port]++;
+    sem_post(init_semaphore(SEM_CARS));
+
+    // Board the ferry
+    sem_wait(init_semaphore(SEM_FERRY));
+    shared_data->waiting_cars[start_port]--;
+    shared_data->ferry_capacity--;
+    fprintf(output, "%d: O %d: boarding\n", shared_data->action_counter++, id);
+    sem_post(init_semaphore(SEM_FERRY));
+
+    // Travel to the destination port
+    int dest_port = (start_port + 1) % 2;
+
+    // Disembark
+    sem_wait(init_semaphore(SEM_FERRY));
+    fprintf(output, "%d: O %d: leaving in %d\n", shared_data->action_counter++, id, dest_port);
+    sem_post(init_semaphore(SEM_FERRY));
+
+    fclose(output);
+}
+
+// Cleanup resources
+void cleanup_resources(SharedData *shared_data) {
+    munmap(shared_data, sizeof(SharedData));
+    shm_unlink("/ferry_shared");
+    cleanup_semaphores();
+}
+
+// Main function
+int main(int argc, char const *argv[]) {
+    srand(time(NULL)); // Seed random number generator
+
+    Config cfg;
+    if (parse_args(argc, argv, &cfg) != EX_SUCCESS) {
+        return EX_ERROR_ARGS;
+    }
+
+    SharedData *shared_data;
+    init_shared_memory(&shared_data);
+    shared_data->ferry_capacity = cfg.capacity;
+
+    sem_t *sem_ferry = init_semaphore(SEM_FERRY);
+    sem_t *sem_cars = init_semaphore(SEM_CARS);
+    sem_t *sem_trucks = init_semaphore(SEM_TRUCKS);
+    sem_t *sem_output = init_semaphore(SEM_OUTPUT);
+
+    pid_t ferry_pid = create_ferry_process(shared_data, &cfg);
+    create_vehicle_processes(shared_data, &cfg);
+
+    waitpid(ferry_pid, NULL, 0);
+
+    cleanup_resources(shared_data);
+    return EX_SUCCESS;
+}
